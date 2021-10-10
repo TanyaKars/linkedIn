4f950654ff048d74fd5a8f79e0faee235bf70c31
tanyakars <euphratus.tk@gmail.com>
2021-10-10 12:49:42 -0700

    clear data

--- cypress/fixtures/data.js
+++
@@ -1,6 +0,0 @@
-const user = {
-    email: "euphratus.tk@gmail.com",
-    password: "linkedin00A!"
-}
-
-module.exports = user;
--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,11 +1,22 @@
-import user from '../fixtures/data'
 
 describe('it should login', () => {
     before(() => {
-        cy.login(user.email, user.password)
+        cy.login(Cypress.env('email'), Cypress.env('password'))
     });
-    it('login',() => {
 
+    // beforeEach(() => {
+    //     Cypress.Cookies.preserveOnce('bscookie', 'spectroscopyId', 'JSESSIONID', 'visit', 'UID', 'DSID', 'aam_uuid');
+    // })
+
+    it('login',() => {
+        cy.get('.search-global-typeahead').type('QA Manager').type('{enter}');
+        cy.get("[aria-label='People']").click();
+        cy.get("[aria-label='Locations filter. Clicking this button displays all Locations filter options.']").contains('Locations').click();
+        cy.get('#geoUrn-103644278').click({force:true});
+        cy.get('#geoUrn-102095887').click({force:true});
+        cy.get('#geoUrn-90000084').click({force:true});
+        cy.get('.artdeco-button__text').contains('Show results').click({force:true});
+            cy.get('.reusable-search__entity-results-list').find('artdeco-button__text').contains('Connect').click()
     })
 
 })
